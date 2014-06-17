6779653fae261261a8dce3037e19820d38e70e01
hamster1987 <january16@tut.by>
2014-06-17 18:14:41 +0300

    запускаем

---
+++ database_details.php
@@ -0,0 +1,5 @@
+<?php//database_details.php
+$username = "first_php_user";
+$password = "first_php_password";
+$hostname = "localhost"; 
+?>
---
+++ setup.php
@@ -0,0 +1,27 @@
+<?php
+require_once 'login.php';
+
+echo 'создаем базу/таблицы<br/>';
+$dbhandle = mysql_connect($hostname, $username, $password)
+  or die('не могу подключиться к mySQl'. mysql_error());
+echo 'подключено к MySQL<br/>';
+$dbname = 'first_php_my_db';
+if (!mysql_query("CREATE DATABASE $dbname", $dbhandle))
+	{ 
+		echo ("не могу создать базу: ". mysql_error(). "<br/>");
+	} else {
+		echo "база создана успешно <br/>";
+			}
+mysql_select_db ($dbname) or die("Не могу выбрать БД $dbname: " . mysql_error());
+echo "база выбрана <br/>";
+if (!mysql_query("CREATE TABLE Users(login CHAR(16), password CHAR(16), PRIMARY KEY (login))", $dbhandle)) 
+	{
+		echo ("не могу создать таблицу: ". mysql_error() . "<br/>");
+	} else {
+		echo "таблица создана <br/>";
+	}
+if (!mysql_close($dbhandle)) die ("не могу закрыть базу:". mysql_error() . "<br/>");
+echo "база закрыта <br/>";
+
+?>
+
